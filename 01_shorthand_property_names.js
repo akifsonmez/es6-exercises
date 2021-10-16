a8d4a85148a31b8dd8c12d0f143f1500017a49ea
asonmez <asonmez@partner.innova.com.tr>
2021-10-16 18:39:43 +0300

    shorthand property names added

---
+++ 01_shorthand_property_names.js
@@ -0,0 +1,12 @@
+const username = "Akif";
+const age = 25;
+
+const user = {username, age};
+console.log(user);
+
+
+/* without shorthand property names
+const user = {
+	username: username,
+	age: age
+}; */
