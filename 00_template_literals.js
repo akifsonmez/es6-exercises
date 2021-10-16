45623adb41443f692fb7ee75031e6814a3bc847c
asonmez <asonmez@partner.innova.com.tr>
2021-10-16 18:22:04 +0300

    template literals added

---
+++ 00_template_literals.js
@@ -0,0 +1,17 @@
+const username = "Akif";
+const helloMessage = `Hello ${username}`;
+// const helloMessage = "Hello " + name; //without template literals
+
+
+console.log(helloMessage);
+
+
+const numberOfBooks = 4;
+const bookPrice = 12;
+const currency = "TL";
+const totalPriceMessage = `Total price is ${bookPrice * numberOfBooks} ${currency}`;
+//const totalPriceMessage = "Total price is " + bookPrice * numberOfBooks + " " + currency; //without template literals
+
+
+console.log(totalPriceMessage);
+
