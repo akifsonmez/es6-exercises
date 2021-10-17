53f3dc99177a3da3bd4429b94f68e0151572d0e8
asonmez <asonmez@partner.innova.com.tr>
2021-10-17 14:00:25 +0300

    import export added

---
+++ 08_import_export/main.js
@@ -0,0 +1,6 @@
+import username, {user, numbers, sayHello} from "./source.js";
+
+console.log(username);
+console.log(user);
+console.log(numbers);
+sayHello();
---
+++ 08_import_export/source.js
@@ -0,0 +1,7 @@
+const username = "Akif";
+const sayHello = () => console.log("Hello");
+const numbers = [1, 2, 3, 4];
+const user = {username: "Akif", age: 25};
+
+export default username;
+export {sayHello, numbers, user};
