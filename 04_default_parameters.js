3ceb2c4d8d2ef38a01e5bf57965fb47db65c9a9a
asonmez <asonmez@partner.innova.com.tr>
2021-10-17 10:28:56 +0300

    default parameters added

---
+++ 04_default_parameters.js
@@ -0,0 +1,11 @@
+const sum = (num1 = 0, num2 = 0) => num1 + num2;
+console.log(sum(2));
+
+//without default parameter
+// const sum = (num1, num2) => {
+// 	if(num1 === undefined) num1 = 0;
+// 	if(num2 === undefined) num2 = 0;
+// 	return num1 + num2;
+// };
+//
+// console.log(sum(2));
