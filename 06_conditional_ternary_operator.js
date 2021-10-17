3400597c83094302468c5cfd1e9b82d487bfeecc
asonmez <asonmez@partner.innova.com.tr>
2021-10-17 13:09:58 +0300

    conditional ternary operator added

---
+++ 06_conditional_ternary_operator.js
@@ -0,0 +1,13 @@
+const username = "Akif";
+
+username === "Akif"
+	? console.log("name is Akif")
+	: username === "Sakif"
+		? console.log("name is Sakif")
+		: console.log("unknown");
+
+// null Nan "" 0 undefined => these return false
+// [] and {} return true
+
+// list.length
+// Object.keys(yourObject).length
